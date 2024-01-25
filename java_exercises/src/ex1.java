c2c795852abe4da5e4f9f1c85d81c0b8c1261df9
Guilherme Pessoa <guipessoagith@gmail.com>
2024-01-25 15:51:27 -0300

    exercicio 2 commit

--- java_exercises/src/ex1.java
+++ java_exercises/src/ex1.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+
+		int senha_correta = 2002;
+		int senha = sc.nextInt();
+
+		while (senha != senha_correta) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+
+		System.out.println("Acesso permitido");
+		sc.close();
 
 	}
 
